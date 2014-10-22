2808414190115133f6ca77d5c4882c26a0aae67a
Sean O'Toole <sotoole@signal.co>
2014-10-22 10:40:59 -0500

    added two tests to the core benchmarking file

---
+++ benchmark_test.go
@@ -0,0 +1,52 @@
+package uas
+
+import "testing"
+
+func benchmarkParser(b *testing.B, expectedName string, ua string) {
+        agent := manifest.Parse(ua)
+        if agent.BrowserVersion.Browser.Name != expectedName {
+                b.FailNow()
+        }
+        for i := 0; i < b.N; i++ {
+                manifest.Parse(ua)
+        }
+}
+
+// first browser regex in list
+func BenchmarkParse_FindOperaMobileBrowser(b *testing.B) {
+        ua := "Mozilla/5.0 (Linux; Android 2.3.4; MT11i Build/4.0.2.A.0.62) AppleWebKit/537.22 " +
+                "(KHTML, like Gecko) Chrome/25.0.1364.123 Mobile Safari/537.22 OPR/14.0.1025.52315"
+        benchmarkParser(b, "Opera Mobile", ua)
+}
+
+// middle of the list browser regex
+func BenchmarkParse_FindCurlBrowserWithForwardSlash(b *testing.B) {
+        benchmarkParser(b, "cURL", "cURL/1.2.3.4")
+}
+
+// /WinHttp/si
+// last browser regex in the list
+func BenchmarkParse_FindLastWinHTTPBrowser(b *testing.B) {
+        benchmarkParser(b, "WinHTTP", "WinHttp 1.2.3.4")
+}
+
+
+func benchmarkParseDevice(b *testing.B, ua string) {
+        // if agent.Device.Regstring != expectedName {
+        //      b.FailNow()
+        // }
+
+        for i := 0; i < b.N; i++ {
+                manifest.Parse(ua)
+        }
+}
+
+// device first in the list
+func BenchmarkParse_FindHbbTVDevice(b *testing.B) {
+        benchmarkParseDevice(b, "/^HbbTV/si")
+}
+
+// device last in list
+func BenchmarkParse_FindMozillaAndroidTabletDevice(b *testing.B) {
+        benchmarkParseDevice(b, "/^Mozilla.*Android.*Tablet.*AppleWebKit/si")
+}
